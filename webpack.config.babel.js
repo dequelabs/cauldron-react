8744cfeedbfb3f0c4bafefbea03310302b4a11eb
Harris Schneiderman <harris.schneiderman@deque.com>
2018-03-02 12:29:03 -0500

    chore: sets libraryTarget

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -6,7 +6,8 @@ module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, './dist/'),
-    filename: 'cauldron.js'
+    filename: 'cauldron.js',
+    libraryTarget: 'commonjs2'
   },
   devtool: isProd ? 'source-map' : 'eval-source-map'
 };
