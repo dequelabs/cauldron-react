8c1f664c121f58a2809fba36d84bb4280992a841
Harris Schneiderman <schne324@gmail.com>
2018-10-26 08:18:08 -0700

    refactor: replaces unique-string with something simpler (#43)

---
+++ test/src/utils/rndid/index.js
@@ -0,0 +1,24 @@
+import test from 'tape';
+import 'test/helpers/setup';
+import rndid from 'src/lib/utils/rndid';
+
+test('utils/rndid', t => {
+  t.test('returns a unique string', t => {
+    let hasDuplicates = false;
+    const ids = new Set();
+
+    for (var i = 0; i < 10000; i++) {
+      const id = rndid();
+
+      if (ids.has(id)) {
+        hasDuplicates = true;
+        break;
+      }
+
+      ids.add(id);
+    }
+
+    t.false(hasDuplicates);
+    t.end();
+  });
+});
