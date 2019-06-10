2bcdad7383c36dee5398696b40a31ae823d7f903
Jason <jason@scurker.com>
2019-06-10 10:30:46 -0500

    remove ref proptype

--- src/components/OptionsMenu/OptionsMenuTrigger.js
+++ src/components/OptionsMenu/OptionsMenuTrigger.js
@@ -20,11 +20,6 @@ function OptionsMenuTriggerComponent({ className, triggerRef, ...other }) {
 OptionsMenuTriggerComponent.propTypes = {
   children: PropTypes.node.isRequired,
   className: PropTypes.string,
-  ref: PropTypes.oneOfType([
-    PropTypes.func,
-    PropTypes.shape({ current: PropTypes.instanceOf(Element) })
-  ]),
-  // I don't think this correct, but it shouldn't be accessible anyway
   triggerRef: PropTypes.oneOfType([
     PropTypes.func,
     PropTypes.shape({ current: PropTypes.instanceOf(Element) })
