1ee360a14f5b11558cbfcb49517abf05d5d207a3
Harris Schneiderman <harris.schneiderman@deque.com>
2019-12-30 13:58:08 -0800

    pure

--- src/components/Link/index.js
+++ src/components/Link/index.js
@@ -1,26 +1,16 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 
-export default class Link extends Component {
-  static propTypes = {
-    children: PropTypes.node,
-    className: PropTypes.string,
-    linkRef: PropTypes.func
-  };
-
-  render() {
-    const { children, linkRef, className, ...other } = this.props;
-    return (
-      <a
-        ref={linkRef}
-        className={classNames('dqpl-link', className)}
-        {...other}
-      >
-        {children}
-      </a>
-    );
-  }
-}
-
+const Link = ({ children, linkRef, className, ...other }) => (
+  <a ref={linkRef} className={classNames('dqpl-link', className)} {...other}>
+    {children}
+  </a>
+);
+Link.propTypes = {
+  children: PropTypes.node,
+  className: PropTypes.string,
+  linkRef: PropTypes.func
+};
 Link.displayName = 'Link';
+export default Link;
